e9bad26f8f8f17f7c0fdf6e9547a6e6e4e5097f5
adel <mhdadel.bablees1@hs-augsburg.de>
2022-11-30 14:45:54 +0100

    add rename method to model and add new entries to gitignore

--- imageStenography/models.py
+++ imageStenography/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 
+def rename_and_path(instance, filename):
+    ext = filename.split('.')[-1]
+#    og_filename = filename.split('.')[0] to get the original name.
+    new_filename = "images/toBeEncrypted/%s.%s (%s)" % (instance.name, ext, instance.id)
+    return new_filename
+
+
 # Create your models here.
 class Image(models.Model):
     name = models.CharField(max_length=200)
-    file = models.ImageField('image', upload_to='images/toBeEncrypted', blank=True, null=True)
+    file = models.ImageField('image', upload_to=rename_and_path, blank=True, null=True)
