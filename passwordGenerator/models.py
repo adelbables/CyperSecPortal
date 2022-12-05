3403f903ff1ae9042e1e16ac3ca0c4d9a137c383
adel <mhdadel.bablees1@hs-augsburg.de>
2022-12-05 14:01:20 +0100

    Add Password model

--- passwordGenerator/models.py
+++ passwordGenerator/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Password(models.Model):
+    password = models.CharField(max_length=60)
+    length = models.IntegerField()
+    use_capital_letters = models.BooleanField
+    use_digit = models.BooleanField
+    use_symbols = models.BooleanField
