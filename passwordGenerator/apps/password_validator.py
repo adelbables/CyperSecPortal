e71a2faed2819e2d81a77f439652b2a11c75de10
adel <mhdadel.bablees1@hs-augsburg.de>
2022-12-05 17:55:28 +0100

    edited the passwords validator

--- passwordGenerator/apps/password_validator.py
+++ passwordGenerator/apps/password_validator.py
@@ -2,14 +2,17 @@ import string
 
 
 def validate_password(passwd):
-    password_strength = {0 :"Weak", 1: "Ok", 2 :"Good", 3:"Strong"}
+    password_strength = {0:"very vulnerable",1 :"Weak", 2: "Ok", 3 :"Good", 4:"Strong"}
     conditions_met = 0
+    small_letters = string.ascii_lowercase
     capital_letters = string.ascii_uppercase
     digits = string.digits
     symbols = string.punctuation
 
     #check if the Password contain min a capital letter, digit or a symbol
     if len(passwd) >= 8:
+        if any(s in passwd for s in small_letters):
+            conditions_met +=1
         if any(s in passwd for s in capital_letters):
             conditions_met +=1
         if any(s in passwd for s in digits):
