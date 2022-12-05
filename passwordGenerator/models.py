33e350c71241c491c2ea7bcd1c8cfcf1e10fa3e0
adel <mhdadel.bablees1@hs-augsburg.de>
2022-12-05 18:56:05 +0100

    adding default value to the boolean fields

--- passwordGenerator/models.py
+++ passwordGenerator/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 
 class Password(models.Model):
-    password = models.CharField(max_length=60)
+    password = models.CharField(max_length=200)
     length = models.IntegerField()
-    use_capital_letters = models.BooleanField
-    use_digits = models.BooleanField
-    use_symbols = models.BooleanField
+    use_capital_letters = models.BooleanField(default=False)
+    use_digits = models.BooleanField(default=False)
+    use_symbols = models.BooleanField(default=False)
