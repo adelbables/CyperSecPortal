0e82f762a85d9db86d998c2f5b24b389971c9176
adelb <994181976#Fh>
2022-12-06 11:47:29 +0100

    remove the id from the renaming function

--- imageStenography/models.py
+++ imageStenography/models.py
@@ -4,9 +4,8 @@ from django.db import models
 def rename_and_path(instance, filename):
     ext = filename.split('.')[-1]
 #    og_filename = filename.split('.')[0] to get the original name.
-    new_filename = "images/toBeEncrypted/%s.%s (%s)" % (instance.name, ext, instance.id)
+    new_filename = "images/toBeEncrypted/%s.%s" % (instance.name, ext)
     return new_filename
-
 
 # Create your models here.
 class Image(models.Model):
