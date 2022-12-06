b78da601ccfeab35cd652f6d7f89d4c132bf13c3
adel <mhdadel.bablees1@hs-augsburg.de>
2022-12-06 09:54:44 +0100

    added encrypt method to upload function

--- imageStenography/views.py
+++ imageStenography/views.py
@@ -1,8 +1,12 @@
+import os
+
 from django import forms
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from CyperSecPortal.settings import MEDIA_ROOT
 from imageStenography import models
+from .apps.encrypt_data_in_image import encrypt_data_into_image
 
 
 # Create your views here.
@@ -22,6 +26,10 @@ def upload(request):
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():  # Formular überprüfen
             form.save()
+            extension = os.path.splitext(request.FILES['file'].name)
+            encrypt_data_into_image(
+                '%s/images/toBeEncrypted/%s%s' % (MEDIA_ROOT, form.cleaned_data['name'], extension[1]),
+                request.POST['secret_text'])
             return HttpResponseRedirect('/upload/')  # Umleitung
     else:
         form = ImageForm()  # leeres Formular
