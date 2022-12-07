3f80f9d8f0d340902570ebd92addbd0a49aab716
adel <mhdadel.bablees1@hs-augsburg.de>
2022-12-07 19:06:48 +0100

    optimized imports for __init__.py

--- imageStenography/apps/__init__.py
+++ imageStenography/apps/__init__.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
 
-from CyperSecPortal.settings import MEDIA_ROOT
-from decrypt_data_from_image import decrypt
-from encrypt_data_in_image import encrypt_data_into_image
-
 
 class ImageStenographyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
