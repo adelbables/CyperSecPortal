1b22b372cc9125158b1fe3fcd2c7ce17e0af333d
adel <mhdadel.bablees1@hs-augsburg.de>
2022-12-08 20:12:58 +0100

    made accounts app and added Login page

--- CyperSecPortal/urls.py
+++ CyperSecPortal/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
                   path('', include('imageStenography.urls')),
                   path('', include('passwordGenerator.urls')),
+                  path('', include('accounts.urls')),
                   path('admin/', admin.site.urls),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
