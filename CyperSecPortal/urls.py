1e057aa6c7b95164102afe522c820dfc2d9ddc43
adelb <994181976#Fh>
2022-12-08 20:47:51 +0100

    added home page

--- CyperSecPortal/urls.py
+++ CyperSecPortal/urls.py
@@ -18,9 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from CyperSecPortal import views
+
 urlpatterns = [
                   path('', include('imageStenography.urls')),
                   path('', include('passwordGenerator.urls')),
                   path('', include('accounts.urls')),
                   path('admin/', admin.site.urls),
+                  path('', views.index)
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
