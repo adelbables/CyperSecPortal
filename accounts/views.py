e47b20b650b265cb34f850d3cf016ecc2b446182
adel <mhdadel.bablees1@hs-augsburg.de>
2022-12-15 11:04:17 +0100

    edited the register method that it works with the new sign-up template

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
@@ -5,11 +6,16 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/')
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        user = User.objects.create_user(username, email, password)
+        user.last_name = last_name
+        user.first_name= first_name
+        return redirect('/')
     else:
         form = UserCreationForm()
-    return render(request, 'registration/register.html', {'form': form})
+    return render(request, 'registration/sign_up.html', {'form': form})
 
