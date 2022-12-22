769ab1bd2367ec7f2557982c58e7db292d766f7e
adel <mhdadel.bablees1@hs-augsburg.de>
2022-12-22 20:22:36 +0100

    added download confirmation message to upload and decrypt

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,8 +1,15 @@
-$(document).ready(function(){
-        $(".files").attr('data-before',"Drag file here or click the above button");
-        $('input[type="file"]').change(function(e){
-            let fileName = e.target.files[0].name;
-            $(".files").attr('data-before',fileName);
+$(document).ready(function () {
+    $(".files").attr('data-before', "Drag file here or click the above button");
+    $('input[type="file"]').change(function (e) {
+        let fileName = e.target.files[0].name;
+        $(".files").attr('data-before', fileName);
 
-        });
     });
+});
+
+$('#encrypt_button').on('click', function (e) {
+    if (!confirm('Your secret was successfully encrypted into the image. confirm download?')) {
+        e.preventDefault();
+    }
+
+});
